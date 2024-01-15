e70280de14acd352dbebed5a5bcd33a717e3d2cc
박수빈 <subin57419@gmail.com>
2024-01-15 22:42:09 +0900

    자료형

---
+++ review_240115/data_type.py
@@ -0,0 +1,19 @@
+string = '문자열'
+integer = 10
+floating = 3.14
+lst = [1, 2, 3, 4, 'a', 'b', [3, 4, '가']]
+dic = {'name': '홍길동', 'age': 20}
+a_set = {1, 2, 3, 4, 5}
+a_range = range(20)
+a_tuple = (1, 2, 3, 4)
+boolean = True
+
+print(type(string))
+print(type(integer))
+print(type(floating))
+print(type(lst))
+print(type(dic))
+print(type(a_set))
+print(type(a_range))
+print(type(a_tuple))
+print(type(boolean))
