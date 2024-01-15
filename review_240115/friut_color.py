e0965598fcd05d2c1491824f82ea1031a5982657
박수빈 <subin57419@gmail.com>
2024-01-15 22:35:16 +0900

    변수 및 출력

---
+++ review_240115/friut_color.py
@@ -0,0 +1,19 @@
+yellow = '바나나'
+red = '딸기'
+green = '수박'
+
+print(f'{yellow}는 노란색이다.')
+print(f'{red}는 빨간색이다.')
+print(f'{green}은 초록색이다.\n')
+
+pink = '복숭아는 분홍색이다.'
+purple = '블루베리는 보라색이다.'
+white = '배는 흰색이다.'
+
+print(pink)
+print(purple)
+print(white)
+
+print('\n\t사과는 "빨간색"이다.')
+print('\t귤은 \'주황색\'이다.')
+print("\t청포도는 \"연두색\"이다.")
