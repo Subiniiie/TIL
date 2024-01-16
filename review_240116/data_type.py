06043efdd15ccb4bfafc654272ba6c743641e37b
박수빈 <subin57419@gmail.com>
2024-01-16 23:40:45 +0900

    절댓값, boolean, 리스트 반복, 정렬 함수

---
+++ review_240116/data_type.py
@@ -0,0 +1,20 @@
+#절댓값을 반환하는 함수
+negative = -3
+print(abs(negative))
+
+#boolean 값 출력
+empty_lst = []
+print(bool(empty_lst))
+
+#주어진 리스트의 모든 값을 더한 값을 출력
+my_list = [1, 2, 3, 4, 5]
+
+sum = 0
+for num in my_list :
+    sum += num
+print(sum)
+
+#주어진 정렬을 오름차순으로 정렬한 결과를 출력
+unsorted_list = ['하', '교', '캅', '의', '지', '가']
+unsorted_list.sort()
+print(unsorted_list)
