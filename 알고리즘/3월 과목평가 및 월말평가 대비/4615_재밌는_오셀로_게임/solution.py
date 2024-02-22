abfe64203a18722b682c637ba954dbb4f375306d
박수빈 <subin57419@gmail.com>
2024-02-22 11:32:41 +0900

    4615_재밌는 오셀로 게임

--- 알고리즘/3월 과목평가 및 월말평가 대비/4615_재밌는_오셀로_게임/solution.py
+++ 알고리즘/3월 과목평가 및 월말평가 대비/4615_재밌는_오셀로_게임/solution.py
@@ -1,2 +1,19 @@
 import sys
 sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1) :
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    visited = [[0 for _ in range(N)] for _ in range(N)]
+    # 1 : B(흑돌), 2 : W(백돌)
+    n = N // 2
+    for _ in range(4) :
+        visited[n][n] = 2
+        visited[n][n-1] = 1
+        visited[n-1][n-1] = 2
+        visited[n-1][n] = 1
+
+
+
