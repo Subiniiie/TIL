0a059187505f0322f70d3026d2e79f1bd8d410e9
박수빈 <subin57419@gmail.com>
2024-02-29 01:14:29 +0900

    10163_색종이

--- 알고리즘/IM/백준/10163_색종이/sol4.py
+++ 알고리즘/IM/백준/10163_색종이/sol4.py
@@ -1,20 +1,18 @@
 N = int(input())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0 for _ in range(101)] for _ in range(101)]
+visited = [[0 for _ in range(1001)] for _ in range(1001)]
+count = []
 
 for k in range(N-1, -1, -1) :
     x, y, w, h = arr[k][0], arr[k][1], arr[k][2], arr[k][3]
-
+    sum_num = 0
     for i in range(x, x+w) :
         for j in range(y, y+h) :
-            if visited[i][j] == 0 :
-                visited[i][j] = k+1
-
-for k in range(N) :
-    sum_num = 0
-    for s in visited :
-        for l in s :
-            if l == k + 1 :
+            if visited[i][j] == 0:
+                visited[i][j] = 1
                 sum_num += 1
-    print(sum_num)
+
+    count.insert(0, sum_num)
+print(*count, sep='\n')
+
