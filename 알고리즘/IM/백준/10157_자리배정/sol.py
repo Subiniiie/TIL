326dd55af3974c0fcbec7dd1071fa4a8cd206f3d
박수빈 <subin57419@gmail.com>
2024-03-02 19:28:51 +0900

    10157_자리배정

--- 알고리즘/IM/백준/10157_자리배정/sol.py
+++ 알고리즘/IM/백준/10157_자리배정/sol.py
@@ -1,85 +1,37 @@
-def col(p, c, r) :
-    global is_status
-    global C
-    global R
-    global result
+def f(x, y) :
+    global seats
 
-    if is_status == True :
-        if p >= R :
-            for i in range(R) :
-                if seats[i][C-c] == 0 :
-                    seats[i][C-c] = 1
-                    p -= 1
-            is_status = False
-            row(p, c+1, r+1)
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
 
+    temp = 1
+    i = 0
+
+    while True :
+        if temp == K :
+            return x + 1, y + 1
+        seats[x][y] = 1
+        temp_x = x + dx[i]
+        temp_y = y + dy[i]
+
+        if 0 <= temp_x < C and 0 <= temp_y < R :
+            if seats[temp_x][temp_y] == 0 :
+                x = temp_x
+                y = temp_y
+                temp += 1
+            else :
+                i = (i + 1) % 4
         else :
-            for i in range(R) :
-                p -= 1
-                if p == 0 :
-                    result.append(i)
+            i = (i + 1) % 4
 
-
-    else  :
-        if p >= R :
-            for i in range(R, -1, -1) :
-                if seats[i][C-c] == 0 :
-                    seats[i][C-c] = 1
-                    p -= 1
-            is_status = True
-            row(p, c-1, r-1)
-
-        else :
-            for i in range(R, -1, -1) :
-                p -= 1
-                if p == 0 :
-                    result.append(i)
-    return result
-
-
-def row(p, c, r):
-    global is_status
-    global C
-    global R
-    global result
-
-    if is_status == True:
-        if p >= C:
-            for j in range(C):
-                if seats[j][R] == 0:
-                    seats[j][R] = 1
-                    p -= 1
-            is_status = False
-            row(p, c + 1, r + 1)
-
-        else:
-            for j in range(C):
-                p -= 1
-                if p == 0:
-                    result.append(j)
-
-    else :
-        if p >= C:
-            for j in range(C, -1, -1):
-                if seats[j][R-r] == 0:
-                    seats[j][R-r] = 1
-                    p -= 1
-            is_status = True
-            row(p, c + 1, r + 1)
-
-        else:
-            for j in range(C, -1, -1):
-                p -= 1
-                if p == 0:
-                    result.append(j)
-    return result
 
 C, R = map(int, input().split())
 K = int(input())
 
-seats = [[0 for _ in range(C)] for _ in range(R)]
-is_status = True
-result = []
-col(K, C, R)
+seats = [[0 for _ in range(1001)] for _ in range(1001)]
 
+if K > C * R :
+    print(0)
+else :
+    print(*f(0, 0))
 
