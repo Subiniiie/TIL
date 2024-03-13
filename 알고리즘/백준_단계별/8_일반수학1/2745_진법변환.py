50758c4f731d813954eadc0dea497d73f2350437
박수빈 <subin57419@gmail.com>
2024-03-13 16:56:58 +0900

    2745_진법변환 하다 맘

--- 알고리즘/백준_단계별/8_일반수학1/2745_진법변환.py
+++ 알고리즘/백준_단계별/8_일반수학1/2745_진법변환.py
@@ -11,11 +11,23 @@ for key in alpha :
 
 arr = list(input().split())
 
-for key in dict.keys() :
-    if key == arr[0][0] :
-        break
+sum_num = 0
+# 10진법을 넘어가는 경우
+if int(arr[1]) > 10 :
+    for i in range(len(arr[0])) :
+        if arr[0][i] in '123456789' :
+            sum_num += int(arr[0][i]) * (int(arr[1]) ** (len(arr[0]) - (i+1)))
+        else :
+            for key in dict.keys() :
+                sum_num
+
+
+
 
 sum_num = 0
 for i in range(len(arr[0])) :
     sum_num += dict[key] * (int(arr[1]) ** i)
 print(sum_num)
+
+
+# 10진법을 넘지 않는 경우
