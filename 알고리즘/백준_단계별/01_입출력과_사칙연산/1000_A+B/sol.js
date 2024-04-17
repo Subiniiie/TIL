cdcda6af58f80dc4b27fe9eb889128a7a773782d
박수빈 <subin57419@gmail.com>
2024-04-17 17:07:57 +0900

    Hello World & A+B

---
+++ 알고리즘/백준_단계별/01_입출력과_사칙연산/1000_A+B/sol.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('',(input) => {
+  const inputData = input.split(' ');
+  const A = parseInt(inputData[0]);
+  const B = parseInt(inputData[1]);
+  console.log(A + B);
+  
+  rl.close();
+});
