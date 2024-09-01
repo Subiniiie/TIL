3ac9893e788f2da6c1b37e13cd77de35c2372018
Subiniiie <subin57419@gmail.com>
2024-09-01 14:41:02 +0900

    날씨앱 요소 배치 및 가로 스크롤

--- 03.ReactNative/NomadWeather/App.js
+++ 03.ReactNative/NomadWeather/App.js
@@ -1,12 +1,83 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, ScrollView } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: 'tomato'}}></View>
-      <View style={{ flex: 1, backgroundColor: 'teal'}}></View>
-      <View style={{ flex: 1, backgroundColor: 'orange'}}></View>
-    </View>
+      <View style={styles.container}>
+        <StatusBar style='light'/>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+        <ScrollView 
+          pagingEnabled
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          // indicatorStyle='white'
+          contentContainerStyle={styles.weather}
+        >
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </ScrollView>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, backgroundColor: 'tomato'
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    color: 'black',
+    fontSize: 68,
+    fontWeight: '500',
+  },
+  weather: {
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
+  },
+  temp: {
+    fontSize: 178,
+    fontWeight: "600",
+    marginTop: 50,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
+  },
+});
