922abe330f99b3fa5c4d47ef92ed6b5b6a2b2f78
Subiniiie <subin57419@gmail.com>
2024-09-08 13:18:35 +0900

    폼 사용성 개선

--- 03.ReactNative/MatzipApp/src/screens/auth/LoginScreen.tsx
+++ 03.ReactNative/MatzipApp/src/screens/auth/LoginScreen.tsx
@@ -1,13 +1,14 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import InputField from '../../components/InputField';
 import CustomButton from '../../components/CustomButton';
 import useForm from '../../hooks/useForm';
 import {validateLogin} from '../../utils';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 const LoginScreen = () => {
-
+  const passwordRef = useRef<TextInput | null>(null);
   const login = useForm({
     initialValue: {email: '', password: ''},
     validate: validateLogin,
@@ -21,18 +22,26 @@ const LoginScreen = () => {
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
           <InputField 
+            autoFocus
             placeholder='이메일'
             error={login.errors.email}
             touched={login.touched.email}
             inputMode='email'
+            returnKeyType='next'
+            blurOnSubmit={false}
+            onSubmitEditing={() => passwordRef.current?.focus()}
             {...login.getTextInputProps('email')}
             >
           </InputField>
           <InputField 
+            ref={passwordRef}
             placeholder='비밀번호' 
             error={login.errors.password}
             touched={login.touched.password}
             secureTextEntry
+            returnKeyType='join'
+            blurOnSubmit={false}
+            onSubmitEditing={handleSubmit}
             {...login.getTextInputProps('password')}
           >
           </InputField>
