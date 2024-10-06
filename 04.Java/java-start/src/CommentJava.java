96b035b1d2a87c4c9b2f89e2564e01650eb0cdf3
Subiniiie <subin57419@gmail.com>
2024-10-06 12:05:30 +0900

    문자열 프린트 및 주석

---
+++ 04.Java/java-start/src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello java1"); //hello java1을 출력한다.
+        // System.out.println("Hello java2");
+
+        /*
+        System.out.println("Hello java3");
+        System.out.println("Hello java4");
+         */
+    }
+}
