d32f3b25da56e457ddd3bcd185de69a504914e0c
Subiniiie <subin57419@gmail.com>
2024-12-29 12:56:12 +0900

    스프링 데이터 JPA

--- 06.강의/01.인프런/02.스프링 입문 - 코드로 배우는 스프링 부트, 웹MVC, DB 접근 기술/hello-spring/src/main/java/hello/hello_spring/repository/JdbcTemplateMemberRepository.java
+++ 06.강의/01.인프런/02.스프링 입문 - 코드로 배우는 스프링 부트, 웹MVC, DB 접근 기술/hello-spring/src/main/java/hello/hello_spring/repository/JdbcTemplateMemberRepository.java
@@ -44,11 +44,6 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
     }
 
     @Override
-    public void clearStore() {
-
-    }
-
-    @Override
     public Optional<Member> findByName(String name) {
         List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
         return result.stream().findAny();
--- 06.강의/01.인프런/02.스프링 입문 - 코드로 배우는 스프링 부트, 웹MVC, DB 접근 기술/hello-spring/src/main/java/hello/hello_spring/repository/JpaMemberRepository.java
+++ 06.강의/01.인프런/02.스프링 입문 - 코드로 배우는 스프링 부트, 웹MVC, DB 접근 기술/hello-spring/src/main/java/hello/hello_spring/repository/JpaMemberRepository.java
@@ -24,11 +24,6 @@ public class JpaMemberRepository implements MemberRepository{
     }
 
     @Override
-    public void clearStore() {
-        em.createQuery("DELETE FROM Member").executeUpdate();
-    }
-
-    @Override
     public Optional<Member> findById(Long id) {
         Member member = em.find(Member.class, id);
         return Optional.ofNullable(member);
