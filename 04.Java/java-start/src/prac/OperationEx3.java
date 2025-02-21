689721cacb303046814b07c241743073be578af0
Subiniiie <subin57419@gmail.com>
2025-02-21 12:19:34 +0900

    연산자 문제 복습

---
+++ 04.Java/java-start/src/prac/OperationEx3.java
@@ -0,0 +1,10 @@
+package prac;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score;
+        score = 66;
+        boolean result = score >= 80 && score <= 100;
+        System.out.println(result);
+    }
+}
