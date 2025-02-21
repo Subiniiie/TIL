e70a6575fc8d65ae7655cbe838951dc88c6feee7
Subiniiie <subin57419@gmail.com>
2025-02-21 12:11:23 +0900

    변수 문제 복습

---
+++ 04.Java/java-start/src/prac/Var8.java
@@ -0,0 +1,15 @@
+package prac;
+
+public class Var8 {
+
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32767;
+        int i = 214748647;
+
+        long l = 9223372036854775807L;
+
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
