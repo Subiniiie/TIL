af415dbbd5f756b1c9609af50bd3ad017fdc6f48
Subiniiie <subin57419@gmail.com>
2026-01-07 23:47:40 +0900

    파이썬 기본 복습

---
+++ 08.2026년/01.파이썬/01.prac.py
@@ -0,0 +1,58 @@
+# 실행 단축키 : py 파일명
+
+# print("Hello world!")
+
+a = 1
+# print(a)
+
+
+"""
+for i in range(4) :
+    print(i)  # i = 0, 1, 2, 3
+    print(a+i)
+"""
+"""
+b = 0
+while b < 10 :
+    print(b)
+    b += 1 
+"""   
+
+"""
+a = "안1녕하세요"
+b = "반1갑습니다"
+
+if "안녕" in a :
+    print("1번")
+elif "반갑" in b :
+    print("2번")
+else :
+    print("없음")
+"""
+
+"""
+myValue = input()
+result = ""
+
+if "1학년" in myValue :
+    result = "저학년"
+elif "4학년" in myValue :
+    result = "고학년"
+else :
+    result = "잘못 입력했습니다."
+    
+print(result)
+"""
+
+"""
+def myFunction() :
+    print('함수 실행')
+    
+myFunction()
+"""
+
+
+def subtractNum() :
+    print(34 - 4)
+    
+subtractNum()
